45fbcd044270e113822705673800e32cb30bc3c2
Kateryna Avramenko <kavramenko2000@gmail.com>
2022-11-02 14:44:05 +0100

    Initial Commit

---
+++ CookAndTaste/CookAndTasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookAndTasteApp.swift
+//  CookAndTaste
+//
+//  Created by Kateryna Avramenko on 02.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CookAndTasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
